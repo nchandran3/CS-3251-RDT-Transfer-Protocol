d80c66224cb7eaa65de15881576c39214a96e821
Naveen Chandran <naveenkchandran@gmail.com>
2014-11-08 15:27:45 -0500

    Created Packet class that will hold all the information necessary for the RDT protocol

---
+++ CS 3251 RDT Protocol/RDTPacket.py
@@ -0,0 +1,22 @@
+"""
+RDT Packet class defined as per the Design Implementation Report
+"""
+
+class RDTPacket():
+    def __init__(self):
+        self.srcIP = None
+        self.srcPort = None
+        self.destIP = None
+        self.destPort = None
+        self.seq_num = None
+        self.ack_num = None
+        self.checksum = None
+        self.length = None
+        self.window = None
+        self.SYN = False
+        self.ACK = False
+        self.TRM = False
+        
+        self.data = None
+    
+    
